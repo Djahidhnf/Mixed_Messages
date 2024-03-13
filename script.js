09148e81e8e151634473d482cac48677e950d27b
Djahid <hnfdjahid@gmail.com>
2024-03-13 19:10:38 +0100

    added a verse object both in english and arabic

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//user input on how they feel
 const prompt = require('prompt-sync')();
+let feeling = prompt('Choose how are you feeling? sad, angry, lost: ');
 
-let feeling = prompt('Choose how are you feeling? Sad, Angry, Lost: ');
+//Object for quran verses in english
+const englishVerses = {
+    sad: ["Allah does not burden a soul beyond that it can bear", "Do not lose hope, nor be sad. You will surely be victorious if you are true believers", "O you who have believed, seek help through patience and prayer. Indeed, Allah is with the patient"],
+    angry: ["My success is not but through Allah. Upon him I have relied, and to Him I return", "And to your Lord direct [your] longing"],
+    lost: ["Whoever puts his trust in Allah; He will be enough for Him"]
+};
+
+const arabicVerses = {
+    sad: ["لَا يُكَلِّفُ اللَّهُ نَفْسًا إِلَّا وُسْعَهَا", "وَلَا تَهِنُوا وَلَا تَحْزَنُوا وَأَنْتُمُ الْأَعْلَوْنَ إِنْ كُنْتُمْ مُؤْمِنِينَ", "يَا أَيُّهَا الَّذِينَ آمَنُواْ اسْتَعِينُواْ بِالصَّبْرِ وَالصَّلاَةِ إِنَّ اللّهَ مَعَ الصَّابِرِينَ"],
+    angry: ["وَمَا تَوْفِيقِي إِلَّا بِاللَّهِ ۚ عَلَيْهِ تَوَكَّلْتُ وَإِلَيْهِ أُنِيبُ", "وَإِلَىٰ رَبِّكَ فَارْغَب"],
+    lost: ["وَمَنْ يَتَوَكَّلْ عَلَى اللَّهِ فَهُوَ حَسْبُهُ"]
+}
+
